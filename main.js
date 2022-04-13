57e09375a2d41d35b3b87023c1441bd4237b4978
William Hobson <will.hobson85@gmail.com>
2022-04-13 16:27:04 -0400

    Add event listener placeholders

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ var searchButton = document.querySelector(".searchbox-button");
 var favoriteButton = document.querySelector(".favorite-button");
 var deleteButton = document.querySelector(".delete-button");
 var commentButton = document.querySelector(".comment-button");
+
+
+filterStarredIdeasBtn.addEventListener("click", );
+showStarredIdeasBtn.addEventListener("click", );
+saveButton.addEventListener("click", );
+searchButton.addEventListener("click", );
+favoriteButton.addEventListener("click", );
+deleteButton.addEventListener("click", );
+commentButton.addEventListener("click", );
