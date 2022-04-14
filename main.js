c67668bd27eba6ad8b6d6438e169d3d5a1958f70
Baskanbetul <btlbskn@hotmail.com>
2022-04-14 14:16:34 -0600

    Check CSS

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-var filterStarredIdeasBtn = document.querySelector(".filter-ideas-button");
-var showStarredIdeasBtn = document.querySelector(".show-ideas-button");
-var titleTextbox = document.querySelector(".idea-name-textbox");
-var bodyTextbox = document.querySelector(".idea-body-textbox");
-var saveButton = document.querySelector(".save-button");
-var searchButton = document.querySelector(".searchbox-button");
-var favoriteButton = document.querySelector(".favorite-button");
-var deleteButton = document.querySelector(".delete-button");
-var commentButton = document.querySelector(".comment-button");
-
-
-filterStarredIdeasBtn.addEventListener("click", );
-showStarredIdeasBtn.addEventListener("click", );
-saveButton.addEventListener("click", );
-searchButton.addEventListener("click", );
-favoriteButton.addEventListener("click", );
-deleteButton.addEventListener("click", );
-commentButton.addEventListener("click", );
+// var filterStarredIdeasBtn = document.querySelector(".filter-ideas-button");
+// var showStarredIdeasBtn = document.querySelector(".show-ideas-button");
+// var titleTextbox = document.querySelector(".idea-name-textbox");
+// var bodyTextbox = document.querySelector(".idea-body-textbox");
+// var saveButton = document.querySelector(".save-button");
+// var searchButton = document.querySelector(".searchbox-button");
+// var favoriteButton = document.querySelector(".favorite-button");
+// var deleteButton = document.querySelector(".delete-button");
+// var commentButton = document.querySelector(".comment-button");
+//
+//
+// filterStarredIdeasBtn.addEventListener("click", );
+// showStarredIdeasBtn.addEventListener("click", );
+// saveButton.addEventListener("click", );
+// searchButton.addEventListener("click", );
+// favoriteButton.addEventListener("click", );
+// deleteButton.addEventListener("click", );
+// commentButton.addEventListener("click", );
