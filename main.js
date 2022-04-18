ef61bf48aead26e65293b8e734b1f67be35ea20d
Patrick Becker <patrickgbecker@gmail.com>
2022-04-18 14:45:56 -0500

    Refactor syntax for import scripts

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+//Global Variables
+var ideas = [];
+
+
+//Query Selectors
 var filterStarredIdeasBtn = document.querySelector(".filter-ideas-button");
 var showStarredIdeasBtn = document.querySelector(".show-ideas-button");
 var titleTextBox = document.querySelector("#idea-name-textbox");
@@ -10,15 +15,23 @@ var deleteButton = document.querySelector(".delete-x");
 var wholeMiniCard = document.querySelector(".idea-grid");
 // var commentButton = document.querySelector("./assets/comment.svg");
 
-var ideas = [];
+//Event Listeners
 
 // filterStarredIdeasBtn.addEventListener("click", );
 // showStarredIdeasBtn.addEventListener("click", );
 saveButton.addEventListener("click", saveIdea);
 // searchButton.addEventListener("click", );
 // favoriteButton.addEventListener("click", );
-// deleteButton.addEventListener("click", );
+deleteButton.addEventListener("click", deleteMiniPoster);
 // commentButton.addEventListener("click", );
+
+//Data Model Functions
+
+
+
+
+//DOM Functions
+
 
 function stopSaveButton() {
   saveButton.disable = true;
@@ -38,10 +51,11 @@ function goSaveButton() {
 
 function saveIdea() {
   event.preventDefault();
-  var idea = new Idea(titleTextBox.value, bodyTextBox.value);
+  var currentIdea = new Idea(titleTextBox.value, bodyTextBox.value);
+  console.log(currentIdea)
   if (titleTextBox.value && bodyTextBox.value) {
-    ideas.unshift(idea);
-    makeMiniPoster(idea);
+    ideas.unshift(currentIdea);
+    makeMiniPoster(currentIdea);
     clearInput();
     goSaveButton();
   } else {
@@ -49,10 +63,19 @@ function saveIdea() {
   }
 }
 
-function makeMiniPoster(idea) {
-  var savedIdeaGrid = `<article class='mini-idea' id=${ideas.id}><div class="mini-header"><img class="save-star" src="./assets/star.svg"><img class="active-star hidden" src="./assets/star-active.svg"><img class="delete-x" src="./assets/delete.svg"></div><div class="mini-body"><h3>${ideas.title}</h3><p>${ideas.body}</p></div><div class="mini-footer"><img src="./assets/comment.svg"><h4>Comment</h4></div></article>`;
+function makeMiniPoster(currentIdea) {
+  var savedIdeaGrid = `<article class="mini-idea" id="${currentIdea.id}"><div class="mini-header"><img class="save-star" src="./assets/star.svg"><img class="active-star hidden" src="./assets/star-active.svg"><img class="delete-x" src="./assets/delete.svg"></div><div class="mini-body"><h3>${currentIdea.title}</h3><p>${currentIdea.body}</p></div><div class="mini-footer"><img src="./assets/comment.svg"><h4>Comment</h4></div></article>`;
     wholeMiniCard.innerHTML += savedIdeaGrid;
-    return wholeMiniCard;
+    return savedIdeaGrid;
+}
+
+function deleteMiniPoster() {
+  if (event.target.className === "delete-x") {
+    var miniPoster = event.target.closest(".mini-idea");
+    var index = ideas.findIndex(idea => idea.id === miniPoster.id)
+    ideas.splice(index, 1);
+    miniPoster.remove()
+  }
 }
 
 function clearInput() {
